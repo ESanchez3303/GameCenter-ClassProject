eeb6bd4c849f428eb7cfeca6446762a3faa355dd
Emanuel Sanchez <emanuelsanchez3303@gmail.com>
2025-08-29 00:22:29 -0500

    first push

---
+++ src/classproject/ClassProject.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package classproject;
+
+/**
+ *
+ * @author Emanuel
+ */
+public class ClassProject {
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(() -> {
+            new MainWindow().setVisible(true);
+        });
+    }
+    
+}
